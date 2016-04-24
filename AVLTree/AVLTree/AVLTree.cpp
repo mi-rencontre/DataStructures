f0e9ebd011ddfbf99c6dfb059eaae54082b05e62
mi-rencontre <15686175080@163.com>
2016-04-24 12:32:52 +0800

    AVLTree

--- AVLTree/AVLTree/AVLTree.cpp
+++ AVLTree/AVLTree/AVLTree.cpp
@@ -32,7 +32,7 @@ public:
 
 	~AVLTree()
 	{
-
+		_destroy(_root);
 	}
 public:
 	bool Insert(const K& key, const V& value)
@@ -231,16 +231,103 @@ protected:
 		parent = subL;
 	}
 
+	//void _RotateRL(Node*& parent)
+	//{
+	//	_RotateR(parent->_right);
+	//	_RotateL(parent);
+	//}
+
+	//void _RotateLR(Node*& parent)
+	//{
+	//	_RotateL(parent->_left);
+	//	_RotateR(parent);
+	//}
+
 	void _RotateRL(Node*& parent)
 	{
-		_RotateR(parent->_right);
-		_RotateL(parent);
+		Node *subR = parent->_right;
+		Node *subRL = subR->_left;
+		subR->_left = subRL->_right;
+
+		if (subRL->_right != NULL)
+		{
+			subRL->_right->_parent = subR;
+		}
+		subRL->_right = subR;
+		subR->_parent = subRL;
+
+		if (subRL->_bf == 0 || subRL->_bf == 1)
+		{
+			subR->_bf = 0;
+		}
+		else
+		{
+			subR->_bf = 1;
+		}
+
+		parent->_right = subRL->_left;
+
+		if (subRL->_left != NULL)
+		{
+			subRL->_left->_parent = parent;
+		}
+		subRL->_left = parent;
+		subRL->_parent = parent->_parent;
+		parent->_parent = subRL;
+		if (subRL->_bf == 0 || subRL->_bf == -1)
+		{
+			parent->_bf = 0;
+		}
+		else
+		{
+			parent->_bf = -1;
+		}
+		parent = subRL;
+		subRL->_bf = 0;
 	}
 
 	void _RotateLR(Node*& parent)
 	{
-		_RotateL(parent->_left);
-		_RotateR(parent);
+		Node *subL = parent->_left;
+		Node *subLR = subL->_right;
+		subL->_right = subLR->_left;
+
+		if (subLR->_left != NULL)
+		{
+			subLR->_left->_parent = subL;
+		}
+		subLR->_left = subL;
+		subL->_parent = subLR;
+
+		if (subLR->_bf == 0 || subLR->_bf == -1)
+		{
+			subL->_bf = 0;
+		}
+		else
+		{
+			subL->_bf = -1;
+		}
+
+		parent->_left = subLR->_right;
+
+		if (subLR->_right != NULL)
+		{
+			subLR->_right->_parent = parent;
+		}
+		subLR->_right = parent;
+		subLR->_parent = parent->_parent;
+		parent->_parent = subLR;
+		if (subLR->_bf == 0 || subLR->_bf == 1)
+		{
+			parent->_bf = 0;
+		}
+		else
+		{
+			parent->_bf = 1;
+		}
+		parent = subLR;
+		subLR->_bf = 0;
+
 	}
 
 	void _InOrder(Node *root)
@@ -253,6 +340,18 @@ protected:
 		cout << root->_key << " ";
 		_InOrder(root->_right);
 	}
+
+	void _destroy(Node* root)
+	{
+		if (root)
+		{
+			_destroy(root->_left);
+			_destroy(root->_right);
+			delete root;
+			root = NULL;
+		}
+	}
+
 private:
 	Node* _root;
 };
@@ -260,7 +359,7 @@ private:
 int main()
 {
 	AVLTree<int, int> at;
-	int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+	int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15};
 	for (int i = 0; i < sizeof(a)/sizeof(a[0]); i++)
 	{
 		at.Insert(a[i], a[i]);
