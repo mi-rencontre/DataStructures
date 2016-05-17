1ed97ef4c15339aaeeb63780c356d1cf96edb96a
mi-rencontre <15686175080@163.com>
2016-05-17 12:01:28 +0800

    GraphMatrix

--- Graph/Graph/GraphMatrix.h
+++ Graph/Graph/GraphMatrix.h
@@ -6,9 +6,10 @@ template<class V, class W>
 class GraphMatrix
 {
 public:
-	GraphMatrix(const V* vArray, int size)
+	GraphMatrix(const V* vArray, int size, bool IsDigraph = false)
 		:_vArray(new V[size])
 		, _vSize(size)
+		, IsDigraph(IsDigraph)
 	{
 		_Matrix = new W*[_vSize];
 		for (int i = 0; i < size; ++i)
@@ -39,8 +40,15 @@ public:
 		assert(srcIndex != -1);
 		assert(dstIndex != -1);
 
-		_Matrix[srcIndex][dstIndex] = weight;
-//		_Matrix[dstIndex][srcIndex] = weight;
+		if (IsDigraph)
+		{
+			_Matrix[srcIndex][dstIndex] = weight;
+		}
+		else
+		{
+			_Matrix[srcIndex][dstIndex] = weight;
+			_Matrix[dstIndex][srcIndex] = weight;
+		}
 	}
 
 	void Display()
@@ -80,6 +88,7 @@ public:
 
 private:
 	V* _vArray;           //顶点集
-	int _vSize;       //顶点数
-	W** _Matrix;        //邻接矩阵
+	int _vSize;          //顶点数
+	W** _Matrix;         //邻接矩阵
+	bool IsDigraph;
 };
